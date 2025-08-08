924163f801047db88349865e241557d883dc5e1b
_LumPq_ <yeonggyu915@gmail.com>
2025-08-08 23:39:06 +0900

    dev

---
+++ nms/v1_20_R2/src/main/java/io/lumpq126/eclipsia/nms/v1_20_R2/NMSHandler_v1_20_R2.java
@@ -0,0 +1,19 @@
+package io.lumpq126.eclipsia.nms.v1_20_R2;
+
+import io.lumpq126.eclipsia.nms.NMSHandler;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class NMSHandler_v1_20_R2 implements NMSHandler {
+    @Override
+    public Inventory createInventory(Player owner, int size, String title) {
+        return Bukkit.createInventory(owner, size, Component.text(title));
+    }
+
+    @Override
+    public void sendActionBar(Player player, String message) {
+        player.sendActionBar(Component.text(message));
+    }
+}
