64e7d1e884cc1dca8c85db6c21188b40d637507f
_LumPq_ <yeonggyu915@gmail.com>
2025-08-11 12:16:17 +0900

    dev

--- core/src/main/java/io/lumpq126/eclipsia/utilities/Mm.java
+++ core/src/main/java/io/lumpq126/eclipsia/utilities/Mm.java
@@ -2,6 +2,7 @@ package io.lumpq126.eclipsia.utilities;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.format.TextDecoration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +14,31 @@ public class Mm {
         return MiniMessage.miniMessage().deserialize(input);
     }
 
+    public static Component mm(String input, boolean bold, boolean italic) {
+        Component comp = MiniMessage.miniMessage().deserialize(input);
+        if (bold) comp = comp.decoration(TextDecoration.BOLD, true);
+        if (italic) comp = comp.decoration(TextDecoration.ITALIC, true);
+        return comp;
+    }
+
     public static List<Component> mm(List<String> input) {
         if (input == null) {
             return new ArrayList<>();
         }
         return input.stream().map(MiniMessage.miniMessage()::deserialize).collect(Collectors.toList());
+    }
+
+    public static List<Component> mm(List<String> input, boolean bold, boolean italic) {
+        if (input == null) {
+            return new ArrayList<>();
+        }
+        return input.stream()
+                .map(s -> {
+                    Component comp = MiniMessage.miniMessage().deserialize(s);
+                    if (bold) comp = comp.decoration(TextDecoration.BOLD, true);
+                    if (italic) comp = comp.decoration(TextDecoration.ITALIC, true);
+                    return comp;
+                })
+                .collect(Collectors.toList());
     }
 }
