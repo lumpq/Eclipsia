73e683e422d7571219be2d8f7deec994445b5ba7
_LumPq_ <yeonggyu915@gmail.com>
2025-08-12 22:44:09 +0900

    init

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,16 +10,16 @@ rootProject.name = "Eclipsia"
 // 기본 모듈
 include("core", "plugin")
 
-// nms 디렉토리 자동 포함
-file("nms").listFiles()
-    ?.filter { it.isDirectory }
-    ?.forEach { dir ->
-        include("nms:${dir.name}")
-    }
+include("nms:v1_20_R1")
+include("nms:v1_20_R2")
+include("nms:v1_20_R3")
+include("nms:v1_20_R4")
+include("nms:v1_21_R1")
+include("nms:v1_21_R2")
+include("nms:v1_21_R3")
+include("nms:v1_21_R4")
+include("nms:v1_21_R5")
 
-// skills 디렉토리 자동 포함 (있다면)
-file("skills").listFiles()
-    ?.filter { it.isDirectory }
-    ?.forEach { dir ->
-        include("skills:${dir.name}")
-    }
+
+include("skills:core")
+include("skills:vampirism")
